4f5a03cd9ba93c4d6b454c3d55d02cb5ddc4e0a9
MA806P <mayp126@126.com>
2017-03-04 16:33:56 +0800

    init project

---
+++ SwiftDemo/SwiftDemo/Calculator/CalculatorViewController.swift
@@ -0,0 +1,62 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by MA806P on 2017/2/23.
+//  Copyright © 2017年 myz. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display!.text!
+            if !(textCurrentlyInDisplay.contains(".") && digit == ".") {
+                display!.text = textCurrentlyInDisplay + digit
+            }
+        } else {
+            if digit == "." {
+                display.text = "0."
+            } else {
+                display.text = digit
+            }
+            userIsInTheMiddleOfTyping = true
+        }
+        
+    }
+    
+    var displayValue : Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        
+        if let mathematicalSymbol = sender.currentTitle {
+            brain.performOperation(mathematicalSymbol)
+        }
+        if let result = brain.result {
+            displayValue = result
+        }
+        
+    }
+
+}
---
+++ SwiftDemo/SwiftDemo/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  SwiftDemo
+//
+//  Created by 159CaiMini02 on 17/3/4.
+//  Copyright © 2017年 myz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "RowCellIdentifier", for: indexPath)
+        cell.textLabel?.text = indexPath.row == 0 ? "Calculator" : "Draw Pic"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            self.navigationController?.pushViewController(CalculatorViewController(), animated: true)
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //self.tableView.register(UINib.init(nibName: "RowCell", bundle: nil), forCellReuseIdentifier: "RowCellIdentifier")
+        self.tableView.register(UITableViewCell.classForCoder(), forCellReuseIdentifier: "RowCellIdentifier")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+        self.title = "SwiftDemo"
+    }
+
+
+}
+
