bbce976d1bc005617718d8b66949778bcbbcdd9a
MA806P <mayp126@126.com>
2017-03-07 20:39:52 +0800

    face demo

---
+++ SwiftDemo/SwiftDemo/FaceIt/FaceView.swift
@@ -0,0 +1,21 @@
+//
+//  FaceView.swift
+//  SwiftDemo
+//
+//  Created by MA806P on 2017/3/7.
+//  Copyright © 2017年 myz. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
