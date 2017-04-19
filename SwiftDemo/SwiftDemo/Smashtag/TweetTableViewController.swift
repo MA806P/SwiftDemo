80033b6c44e218d61988df897ec83c543371fdc8
MA806P <mayp126@126.com>
2017-04-19 17:41:46 +0800

    update

--- SwiftDemo/SwiftDemo/Smashtag/TweetTableViewController.swift
+++ SwiftDemo/SwiftDemo/Smashtag/TweetTableViewController.swift
@@ -7,12 +7,12 @@
 //
 
 import UIKit
-import Twitter
+//import Twitter
 
 class TweetTableViewController: UITableViewController {
     
     
-    private var tweets = [Array<Twitter.tweet>]()
+    //private var tweets = [Array<Twitter.tweet>]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
