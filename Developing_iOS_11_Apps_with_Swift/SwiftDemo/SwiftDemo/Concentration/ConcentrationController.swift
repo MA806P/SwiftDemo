0d704cc511ee84d8cd11c676a5d840aaa14b45e0
MA806P <mayp126@126.com>
2017-11-27 22:33:34 +0800

    concentration add constraint

--- Developing_iOS_11_Apps_with_Swift/SwiftDemo/SwiftDemo/Concentration/ConcentrationController.swift
+++ Developing_iOS_11_Apps_with_Swift/SwiftDemo/SwiftDemo/Concentration/ConcentrationController.swift
@@ -63,10 +63,14 @@ class ConcentrationController: UIViewController {
     
     func emoji(for card: Card) -> String {
         
+        print(" \(card.identifier)  \(emojiChoices)")
+        
         if emoji[card.identifier] == nil , emojiChoices.count > 0 {
             let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
             emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
         }
+        
+        print("\(emojiChoices)")
         
         return emoji[card.identifier] ?? "?"
     }
