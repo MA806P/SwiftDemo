4e0d17f71def197ea87396837d13715b1f9c53fc
MA806P <mayp126@126.com>
2018-07-26 15:48:21 +0800

    update

--- SwiftTestDemo/SwiftTestDemo/main.swift
+++ SwiftTestDemo/SwiftTestDemo/main.swift
@@ -397,18 +397,22 @@ import Foundation
 
 //--------------- Closures -----------------
 
-func backward(_ s1: String, _ s2: String) -> Bool {
-    return s1 > s2
-}
-let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
-var reNames = names.sorted(by: backward)
-print(reNames)
+//func backward(_ s1: String, _ s2: String) -> Bool {
+//    return s1 > s2
+//}
+//let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+//var reNames = names.sorted(by: backward)
+//print(reNames)
+//
+//reNames = names.sorted(by: { (s1: String, s2: String) -> Bool in return s1 > s2 })
+//reNames = names.sorted(by: { s1, s2 in return s1 > s2 })
+//reNames = names.sorted(by: { s1, s2 in s1 > s2 } )
+//reNames = names.sorted(by: { $0 > $1 } )
+//reNames = names.sorted(by: >)
 
-reNames = names.sorted(by: { (s1: String, s2: String) -> Bool in return s1 > s2 })
-reNames = names.sorted(by: { s1, s2 in return s1 > s2 })
-reNames = names.sorted(by: { s1, s2 in s1 > s2 } )
-reNames = names.sorted(by: { $0 > $1 } )
-reNames = names.sorted(by: >)
+
+
+
 
 //--------------- Objects and Classes -----------------
 
