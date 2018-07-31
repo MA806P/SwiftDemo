8492d5134aa2ddcf60810f3a17ef6fe23351f685
MA806P <mayp126@126.com>
2018-07-31 21:33:02 +0800

    update

--- SwiftTestDemo/SwiftTestDemo/Enumerations.swift
+++ SwiftTestDemo/SwiftTestDemo/Enumerations.swift
@@ -40,6 +40,18 @@ class EnumerationsTestObject {
         
         
         
+//        directionToHead = .south
+//        switch directionToHead {
+//        case .north:
+//            print("Lots of planets have a north")
+//        case .south:
+//            print("Watch out for penguins")
+//        case .east:
+//            print("Where the sun rises")
+//        case .west:
+//            print("Where the skies are blue")
+//        }
+//        // Prints "Watch out for penguins"
         
     }
     
