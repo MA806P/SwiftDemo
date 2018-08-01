1a8b0f988866d7d5681040f34da0a4db5d1ab706
MA806P <mayp126@126.com>
2018-08-01 23:30:02 +0800

    update

--- SwiftTestDemo/SwiftTestDemo/Enumerations.swift
+++ SwiftTestDemo/SwiftTestDemo/Enumerations.swift
@@ -14,44 +14,38 @@ class EnumerationsTestObject {
     func enmerationsDemo() {
         
         
-        enum Rank: Int {
-            case ace = 1
-            case two, three, four, five, six, seven, eight, nine, ten
-            case jack, queen, king
-            func simpleDescription() -> String {
-                switch self {
-                case .ace:
-                    return "ace"
-                case .jack:
-                    return "jack"
-                case .queen:
-                    return "queen"
-                case .king:
-                    return "king"
-                default:
-                    return String(self.rawValue)
-                }
-            }
-        }
-        let ace = Rank.ace
-        let aceRawValue = ace.rawValue
-        print("\(ace), \(aceRawValue), \(ace.simpleDescription()), \(Rank(rawValue: 3) ?? Rank.king)")
-        //ace, 1, ace, three
-        
-        
-        
-//        directionToHead = .south
-//        switch directionToHead {
-//        case .north:
-//            print("Lots of planets have a north")
-//        case .south:
-//            print("Watch out for penguins")
-//        case .east:
-//            print("Where the sun rises")
-//        case .west:
-//            print("Where the skies are blue")
+//        enum Rank: Int {
+//            case ace = 1
+//            case two, three, four, five, six, seven, eight, nine, ten
+//            case jack, queen, king
+//            func simpleDescription() -> String {
+//                switch self {
+//                case .ace:
+//                    return "ace"
+//                case .jack:
+//                    return "jack"
+//                case .queen:
+//                    return "queen"
+//                case .king:
+//                    return "king"
+//                default:
+//                    return String(self.rawValue)
+//                }
+//            }
 //        }
-//        // Prints "Watch out for penguins"
+//        let ace = Rank.ace
+//        let aceRawValue = ace.rawValue
+//        print("\(ace), \(aceRawValue), \(ace.simpleDescription()), \(Rank(rawValue: 3) ?? Rank.king)")
+//        //ace, 1, ace, three
+        
+        
+    
+//        enum Beverage: CaseIterable {
+//            case coffee, tea, juice
+//        }
+//        let numberOfChoices = Beverage.allCases.count
+//        print("\(numberOfChoices) beverages available")
+//        // Prints "3 beverages available"
         
     }
     
