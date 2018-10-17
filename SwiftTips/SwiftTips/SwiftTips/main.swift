6b864d0533dfc806f1de31c144987343394e0a6f
MA806P <mayp126@126.com>
2018-10-17 10:17:30 +0800

    方法嵌套。命名空间

--- SwiftTips/SwiftTips/SwiftTips/main.swift
+++ SwiftTips/SwiftTips/SwiftTips/main.swift
@@ -13,9 +13,73 @@ print("Hello, World!")
 
 // -----------------------------
 
+//命名空间
+/*
+ 在 Swift 中，由于可以使用命名空间了，即使是名字相同的类型，只要是来自不同的命名空间的话，都是可以和平共处的。
+ 和 C# 这样的显式在文件中指定命名空间的做法不同，Swift 的命名空间是基于 module 而不是在代码中显式地指明，
+ 每个 module 代表了 Swift 中的一个命名空间。也就是说，同一个 target 里的类型名称还是不能相同的。
+ 在我们进行 app 开发时，默认添加到 app 的主 target 的内容都是处于同一个命名空间中的，
+ 我们可以通过创建 Cocoa (Touch) Framework 的 target 的方法来新建一个 module，
+ 这样我们就可以在两个不同的 target 中添加同样名字的类型了：
+
+ // MyFramework.swift
+ // 这个文件存在于 MyFramework.framework 中
+ public class MyClass { public class func hello() { print("hello from framework") } }
+ 
+ // MyApp.swift
+ // 这个文件存在于 app 的主 target 中
+ class MyClass { class func hello() { print("hello from app") } }
+ 
+ //在使用时，如果出现可能冲突的时候，我们需要在类型名称前面加上 module 的名字 (也就是 target 的名字)：
+ MyClass.hello()
+ // hello from app
+ 
+ MyFramework.MyClass.hello()
+ // hello from framework”
+ 
+ */
+
+
+//另一种策略是使用类型嵌套的方法来指定访问的范围。常见做法是将名字重复的类型定义到不同的 struct 中，以此避免冲突。
+//这样在不使用多个 module 的情况下也能取得隔离同样名字的类型的效果：
+struct MyClassContainer1 {
+    class MyClass {
+        class func hello() {
+            print("hello from MyClassContainer1")
+        }
+    }
+}
+
+struct MyClassContainer2 {
+    class MyClass {
+        class func hello() {
+            print("hello from MyClassContainer2")
+        }
+    }
+}
+MyClassContainer1.MyClass.hello()
+MyClassContainer2.MyClass.hello()
+
+
+
+// -----------------------------
+/*
+ 方法嵌套
+ “我们可以将方法当作变量或者参数来使用了。更进一步地，我们甚至可以在一个方法中定义新的方法，
+ 这给代码结构层次和访问级别的控制带来了新的选择。
+ 
+ “我们可以在方法中定义其他方法，也就是说让方法嵌套起来。
+ 
+ */
+
+
+
+
+// -----------------------------
+
+/*
 //下标
 //想要一下取出数组中几个元素
-
 extension Array {
     subscript(input: [Int]) -> ArraySlice<Element> {
         get {
@@ -41,7 +105,7 @@ var result = array[[0, 2, 4]]
 print(result) //[0, 2, 4]
 array[[0, 2, 4]] = [-1, -2, -3]
 print(array) //[-1, 1, -2, 3, -3, 5]
-
+*/
 
 // -----------------------------
 
