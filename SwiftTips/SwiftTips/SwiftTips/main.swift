6a1d0db1793f615385920bde7bc9a4f7cd46a151
MA806P <mayp126@126.com>
2018-10-19 09:46:48 +0800

    可变参函数

--- SwiftTips/SwiftTips/SwiftTips/main.swift
+++ SwiftTips/SwiftTips/SwiftTips/main.swift
@@ -10,8 +10,56 @@ import Foundation
 
 print("Hello, World!")
 
+
 // -----------------------------
 
+//可变参函数，可接受人一多个参数的函数，如 stringWithFormat:
+
+func sum(input: Int ...) -> Int {
+    return input.reduce(0, +);
+}
+print(sum(input: 1,2,3)) //6
+//输入的 input 在函数体内部将被作为数组 [Int] 来使用
+
+
+/*
+ 在其他很多语言中，因为编译器和语言自身语法特性的限制，
+ 在使用可变参数时往往可变参数只能作为方法中的最后一个参数来使用，
+ 而不能先声明一个可变参数，然后再声明其他参数。这是很容易理解的，
+ 因为编译器将不知道输入的参数应该从哪里截断。这个限制在 Swift 中是不存在的，
+ 因为我们会对方法的参数进行命名，所以我们可以随意地放置可变参数的位置，而不必拘泥于最后一个参数
+ 
+ */
+func myFunc(numbers: Int ..., string: String) {
+    numbers.forEach {
+        for i in 0..<$0 {
+            print("\(i+1): \(string)")
+        }
+    }
+}
+myFunc(numbers: 1, 2, 3, string: "hello")
+//1: hello
+//1: hello
+//2: hello
+//1: hello
+//2: hello
+//3: hello
+
+
+
+/*
+ 限制自然是有的，比如在同一个方法中只能有一个参数是可变的，比如可变参数都必须是同一种类型的等.当我们想要同时传入多个类型的参数时就需要做一些变通。比如最开始提到的 -stringWithFormat: 方法。可变参数列表的第一个元素是等待格式化的字符串，在 Swift 中这会对应一个 String 类型，而剩下的参数应该可以是对应格式化标准的任意类型。一种解决方法是使用 Any 作为参数类型，然后对接收到的数组的首个元素进行特殊处理。不过因为 Swift 提供了使用下划线 _ 来作为参数的外部标签，来使调用时不再需要加上参数名字。我们可以利用这个特性，在声明方法时就指定第一个参数为一个字符串，然后跟一个匿名的参数列表，这样在写起来的时候就 "好像" 是所有参数都是在同一个参数列表中进行的处理，会好看很多。比如 Swift 的 NSString 格式化的声明就是这样处理的：
+ 
+ extension NSString { convenience init(format: NSString, _ args: CVarArgType...) }
+ */
+let string = NSString(format: "hello %@. date %@", "Tom", NSDate())
+print(string) //hello Tom. date 2018-10-19 01:40:20 +0000
+
+
+// -----------------------------
+
+
+/*
 //typealias
 //typealias是用来为已经存在的类型重新定义名字的，通过命名可以使代码变的更加清晰。
 
@@ -121,6 +169,7 @@ func isDangerous<T: Animal>(animal: T) -> Bool {
 //isDangerous(animal: Tiger()) //true
 //isDangerous(animal: Sheep()) //false
 
+ */
 
 // -----------------------------
 
