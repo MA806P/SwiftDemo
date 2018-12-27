5f7cac316a2720ae52cd6f9e1f9fe479b6418218
MA806P <mayp126@126.com>
2018-12-27 08:59:37 +0800

    kvo update

--- SwiftTips/SwiftTips/OCToSwift/main.swift
+++ SwiftTips/SwiftTips/OCToSwift/main.swift
@@ -24,10 +24,56 @@ print("Hello, World!")
  “在 Swift 中我们也是可以使用 KVO 的，不过 KVO 仅限于在 NSObject 的子类中，因为 KVO 是基于 KVC (Key-Value Coding)
  以及动态派发技术实现的，而这些东西都是 OC 运行时的概念。另外由于 Swift 为了效率，默认禁用了动态派发，
  因此想用 Swift 来实现 KVO，我们还需要做额外的工作，那就是将想要观测的对象标记为 dynamic 和 @objc。”
- 
- 
- 
  */
+
+
+
+
+
+class MyClass: NSObject {
+    @objc dynamic var date = Date()
+}
+
+private var myContext = 0
+
+class Class: NSObject {
+    
+    var myObject: MyClass!
+    
+    override init() {
+        super.init()
+        myObject = MyClass()
+        print("初始化 MyClass，当前日期: \(myObject.date)")
+        myObject.addObserver(self,
+                             forKeyPath: "date",
+                             options: .new,
+                             context: &myContext)
+        
+        delayBling(3) {
+            self.myObject.date = Date()
+            
+        }
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?,
+                               of object: Any?,
+                               change: [NSKeyValueChangeKey : Any]?,
+                               context: UnsafeMutableRawPointer?)
+    {
+        if let change = change, context == &myContext {
+            if let newDate = change[.newKey] as? Date {
+                print("MyClass 日期发生变化 \(newDate)")
+            }
+        }
+
+    }
+}
+
+let obj = Class()
+        
+        
+
+
 
 
 // -----------------------------
