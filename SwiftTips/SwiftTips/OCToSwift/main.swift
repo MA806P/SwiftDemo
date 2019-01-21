e245dfe0e67aefc330bc3064c3c4f74854cf0f4a
MA806P <mayp126@126.com>
2019-01-21 10:09:30 +0800

    delegate

--- SwiftTips/SwiftTips/OCToSwift/main.swift
+++ SwiftTips/SwiftTips/OCToSwift/main.swift
@@ -14,6 +14,34 @@ print("Hello, World!")
 
 // -----------------------------
 
+// delegate
+/*
+ Cocoa 开发，在 ARC 中，对于一般的 delegate，我们会在声明中将其指定为 weak，
+ 在这个 delegate 实际的对象被释放的时候，会被重置回 nil。这可以保证即使 delegate 已经不存在时，
+ 我们也不会由于访问到已被回收的内存而导致崩溃。ARC 的这个特性杜绝了 Cocoa 开发中一种非常常见的崩溃错误
+ 
+ 
+ Swift 的 protocol 是可以被除了 class 以外的其他类型遵守的，而对于像 struct 或是 enum 这样的类型，
+ 本身就不通过引用计数来管理内存，所以也不可能用 weak 这样的 ARC 的概念来进行修饰。
+ 
+ 想要在 Swift 中使用 weak delegate，我们就需要将 protocol 限制在 class 内。
+ 一种做法是将 protocol 声明为 Objective-C 的，这可以通过在 protocol 前面加上 @objc 关键字来达到，
+ Objective-C 的 protocol 都只有类能实现，因此使用 weak 来修饰就合理了
+ @objc protocol MyClassDelegate { func method() }
+ 
+ 另一种可能更好的办法是在 protocol 声明的名字后面加上 class，这可以为编译器显式地指明这个 protocol 只能由 class 来实现。
+ protocol MyClassDelegate: class { func method() }
+ 
+ 后一种方法更能表现出问题的实质，同时也避免了过多的不必要的 Objective-C 兼容，可以说是一种更好的解决方式
+ 
+ 例子见 SwiftAppTest
+ 
+ */
+
+
+
+// -----------------------------
+
 //C 代码调用 和 @asmname
 
 /*
@@ -26,11 +54,11 @@ print("Hello, World!")
  对于第三方的 C 代码，想要调用非标准库的 C 代码的话，将 C代码的头文件在桥接的头文件中进行导入
  详情见 target SwiftToOCExample
  
- 
+ print(sin(Double.pi/2)) //1.0
  
  */
 
-print(sin(Double.pi/2)) //1.0
+
 
 
 // -----------------------------
--- SwiftTips/SwiftTips/SwiftAppTest/ViewController.swift
+++ SwiftTips/SwiftTips/SwiftAppTest/ViewController.swift
@@ -8,37 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MyClassDelegate {
+    
+    var myClass: MyClass!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        let myClass: MyClass = MyClass()
+        myClass.delegate = self
+        
+    }
+    
+    func delegateMethod() {
+        print("MyClassDelegate Method")
+    }
+    
+    
+    
+    @IBAction func delayCallAction(_ sender: Any) {
+        
         print(Date())
-
+        
         // GCD 延时调用
         let time: TimeInterval = 2.0
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + time) {
             print(Date())
             print("2 秒后输出")
         }
-
-
-
+        
         //封装的方法
         let task = delayBling(5, task: {print("do something")})
         //取消
         canelBling(task)
-        
-        
-        
     }
     
     
     
-    
-    
-    
+    @IBAction func delegateCallAction(_ sender: UIButton) {
+        self.delegateMethod()
+    }
     
 
     override func didReceiveMemoryWarning() {
@@ -46,6 +56,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+}
+
+
+protocol MyClassDelegate: class {
+    func delegateMethod()
+}
+
+class MyClass {
+    weak var delegate: MyClassDelegate?
 }
 
 
