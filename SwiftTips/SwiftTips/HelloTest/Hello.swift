eddd2668545595cf4922d9abcdb87735d7c50834
MA806P <mayp126@126.com>
2019-02-16 16:14:29 +0800

    Framework test

---
+++ SwiftTips/SwiftTips/HelloTest/Hello.swift
@@ -0,0 +1,17 @@
+//
+//  Hello.swift
+//  HelloTest
+//
+//  Created by MA806P on 2019/2/16.
+//  Copyright © 2019 myz. All rights reserved.
+//
+
+import Foundation
+
+public class HelloFace {
+    public class func faceSayHello() {
+        print("Face Say Hello")
+    }
+}
+
+
---
+++ SwiftTips/SwiftTips/HelloTest/HelloTest.h
@@ -0,0 +1,19 @@
+//
+//  HelloTest.h
+//  HelloTest
+//
+//  Created by MA806P on 2019/2/16.
+//  Copyright © 2019 myz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HelloTest.
+FOUNDATION_EXPORT double HelloTestVersionNumber;
+
+//! Project version string for HelloTest.
+FOUNDATION_EXPORT const unsigned char HelloTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloTest/PublicHeader.h>
+
+
